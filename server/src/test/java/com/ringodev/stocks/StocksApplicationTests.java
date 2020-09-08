7455836c3b4ed96f5648f1e95ee439048cc7d967
ringodev <ringodev@protonmail.com>
2020-09-08 20:41:58 +0200

    initial commit

---
+++ server/src/test/java/com/ringodev/stocks/StocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ringodev.stocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
