90b75c7f54b8d4f36fc6f4b292e625d21fb912e3
ringodev <ringodev@protonmail.com>
2020-10-20 13:27:55 +0200

    tried different cors setting

--- src/main/java/com/ringodev/stocks/controller/GatewayController.java
+++ src/main/java/com/ringodev/stocks/controller/GatewayController.java
@@ -16,10 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
